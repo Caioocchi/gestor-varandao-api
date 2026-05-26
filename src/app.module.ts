b0931032664c9e423325ba8538b6523c3d1e1ca5
Caio Occhi <caioocchi5@gmail.com>
2026-05-26 07:03:23 -0300

    fix: Criação do arquivo .env e ajuste no app.module para buscar a URI direto do .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FreelaModule } from './module/freela.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://varandao:Varandao2026@ac-c5sbqiy-shard-00-00.8e4dtqg.mongodb.net:27017,ac-c5sbqiy-shard-00-01.8e4dtqg.mongodb.net:27017,ac-c5sbqiy-shard-00-02.8e4dtqg.mongodb.net:27017/?ssl=true&replicaSet=atlas-7xsur3-shard-0&authSource=admin&appName=varandao',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+
+      useFactory: (
+        configService: ConfigService,
+      ) => ({
+        uri: configService.get<string>(
+          'MONGODB_URI'
+        )
+      })
+    }),
     FreelaModule
   ]
 })
