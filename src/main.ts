45e3539c7dc46853da1ef605c2b3cc89388889ff
Caio Occhi <caioocchi5@gmail.com>
2026-05-26 13:20:56 -0300

    fix: inclusão de outro localhost no enableCors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     );
 
   app.enableCors({
-    origin: 'http://localhost:9000',
+    origin: ['http://localhost:9000', 'http://localhost:9200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
